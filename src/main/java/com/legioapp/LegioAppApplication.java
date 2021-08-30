02638b98cda41a4eb2c82f947c84f312c10b3053
André Carvalho <andre_carvalho0@live.com>
2021-08-30 09:50:23 -0300

    Create new project

---
+++ src/main/java/com/legioapp/LegioAppApplication.java
@@ -0,0 +1,13 @@
+package com.legioapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegioAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legioapp/LegioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legioapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
